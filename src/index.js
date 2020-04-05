e010d76ba36d64d6e42672a0ee137d4030b4acf4
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-05 14:59:31 +0300

    added a handle click

--- src/index.js
+++ src/index.js
@@ -30,6 +30,12 @@ class Board extends React.Component{
     };
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i){
     return <Square
     value={this.state.squares[1]}
