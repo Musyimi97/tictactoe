477e46071e564eab48d574beb86b43f885568bbc
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-05 14:56:06 +0300

    some moree components from square

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ class Square extends React.Component{
 
   render(){
     return(
-      <button className="square"
+      <button 
+      className="square"
       onClick={() =>this.props.onClick()}
       >
         {this.props.value}
