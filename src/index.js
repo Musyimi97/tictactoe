d426d77f0ce84b359e840d72742ea284874e7b39
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-05 16:15:35 +0300

    indefinate movemment we ahve a winner X

--- src/index.js
+++ src/index.js
@@ -24,23 +24,7 @@ class Board extends React.Component{
     };
   }
 
-  handleClick(i){
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-      
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      hsitory: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
-    });
-   
-  }
+
 
   renderSquare(i){
     return <Square
@@ -88,6 +72,23 @@ class Game extends React.Component{
         stepNumber:0,
         xIsNext:true,
       }
+    }
+
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
     }
     
     jumpTo(step){
